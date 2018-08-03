9d0d102c1b0f7f18da49b1eef1596c7f7b3c9242
Matt Farmer <mrfarmer777@gmail.com>
2018-08-03 01:53:05 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,6 +19,7 @@ class ProductsController < ApplicationController
 
   def create
     Product.create(product_params)
+    #give back some json, because we're creating it with AJAX
     render json: @product, status: 201
   end
 
